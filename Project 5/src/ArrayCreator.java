de138faeed343abefd92e162a6e298937336a153
jkreier <jkreier@skidmore.edu>
2014-10-28 16:24:25 -0400

    more work done on pilot program

--- Project 5/src/ArrayCreator.java
+++ Project 5/src/ArrayCreator.java
@@ -1,4 +1,27 @@
+/**
+ * ArrayCreator makes the array of Waypoint objects and gives them values
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class ArrayCreator {
-
+	public void load(String filepath) throws FileNotFoundException  {
+		File file = new File (filepath);
+		
+	}
+	
+	
+	public int numLines(String filepath) throws FileNotFoundException{	
+	Scanner fileReader = new Scanner(new File(filepath)); 
+	int numberofLines = 0;
+		while (fileReader.hasNextLine()){
+			numberofLines ++;
+			fileReader.nextLine();
+			}
+	fileReader.close();
+	return numberofLines;
+	}
 }
--- Project 5/src/Waypoint.java
+++ Project 5/src/Waypoint.java
@@ -1,7 +1,19 @@
 
+/**
+ * this class builds the object that each waypoint will be made of.
+ * it contains the values for the name of the waypoint, the heading
+ * and the distance to the next waypoint.
+ * @author jake
+ *
+ */
 public class Waypoint {
 	public String waypoint;
 	public int heading;
 	public double distance;
-	
+	//construct the object:
+	public Waypoint(String waypoint, int heading, double distance){
+		this.waypoint = waypoint;
+		this.heading = heading;
+		this.distance = distance;
+	}
 }
