9020d06c9d9f2c79c64120d3db02897182f3eb65
jkreier <jkreier@skidmore.edu>
2014-10-29 19:33:57 -0400

    commit

--- Project 5/src/ArrayCreator.java
+++ Project 5/src/ArrayCreator.java
@@ -8,12 +8,25 @@ import java.util.Scanner;
 
 
 public class ArrayCreator {
-	public void load(String filepath) throws FileNotFoundException  {//TODO figure out how to pass the file name from the main class
-	Scanner fileReader = new Scanner(filepath);
-	int numLines = numLines(filepath); //does this recognize that I'm trying to call a new method?
-	Waypoint[] Waypoint = new Waypoint[numLines];//defines the Waypoint object-array and gives it the number of waypoints.
-	
-	
+	public void load(String filepath) throws FileNotFoundException  {
+		Scanner fileReader = new Scanner(filepath);
+		int numLines = numLines(filepath); //does this recognize that I'm trying to call a new method?
+		Waypoint[] waypoints = new Waypoint[numLines];//defines the Waypoint object-array and gives it the number of waypoints.
+		int i = 0;
+		while (i < numLines){
+			waypoints [i] = new Waypoint( fileReader.next() , fileReader.nextInt(), fileReader.nextDouble()); //populate the array
+			i++; 
+			}
+		//TODO take array and add the last double of each iteration to 
+		/*double totalDistance = 0;
+		int n = 0;
+		while (n < numLines){
+		totalDistance += waypoints[n].nextDouble; 
+		}*/
+		// do I need to return totalDistance to the main class or can I print out the totalDistance in ArrayCreator?
+		System.out.println("sample line" + waypoints[2]);
+		
+		
 	
 	
 		
